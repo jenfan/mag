a25a9a8ad5b5ce0a8e9bc6bfc8967fbf2dd2c60d
Gregor <am@iworry.ru>
2014-02-10 00:08:17 +0400

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mag::Application.config.secret_key_base = '6f9fb7e635cedfb2dd9459e361d37b41429d7b0d001c2105db1edfba29220fd253d5df96c6a237a35cbc01ca883ecf9f67aff84d0279046297e03ae9fe5f8127'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mag::Application.config.session_store :cookie_store, key: '_mag_session'
